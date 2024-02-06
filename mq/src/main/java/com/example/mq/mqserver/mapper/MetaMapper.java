7dca965125ca797aad4dec0d8a46ea8d8e35a8f0
bit-yidan <1256970054@qq.com>
2024-02-06 20:36:01 +0800

    实现数据库表的插入和删除

--- mq/src/main/java/com/example/mq/mqserver/mapper/MetaMapper.java
+++ mq/src/main/java/com/example/mq/mqserver/mapper/MetaMapper.java
@@ -1,5 +1,8 @@
 package com.example.mq.mqserver.mapper;
 
+import com.example.mq.mqserver.core.Binding;
+import com.example.mq.mqserver.core.Exchange;
+import com.example.mq.mqserver.core.MSGQueue;
 import org.apache.ibatis.annotations.Mapper;
 
 /**
@@ -16,4 +19,14 @@ public interface MetaMapper {
     void createExchangeTable();
     void createQueueTable();
     void createBindingTable();
+
+    //针对上述三个基本概念进行插入和删除
+    void insertExchange(Exchange exchange);
+    void deleteExchange(String exchangeName);
+    void insertQueue(MSGQueue queue);
+    void deleteQueue(String queueName);
+    void insertBinding(Binding binding);
+    void deleteBinding(Binding binding);
+
+
 }
